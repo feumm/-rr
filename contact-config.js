11ef9d88c2a2f4966c8a49b5c4ce437a18fb1e29
eufm <hundredrackies@gmail.com>
2026-09-21 01:53:01 +0700

    Add files via upload

---
+++ contact-config.js
@@ -0,0 +1,2 @@
+// Paste your deployed Google Apps Script web app URL between the quotes.
+window.PORTFOLIO_CONTACT_ENDPOINT = '';
---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
+  );
+  self.registration.unregister();
+  self.clients.claim();
+});
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
+});
